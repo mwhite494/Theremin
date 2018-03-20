c5db348c8929ffe366a46a44084623812a88b6c8
Marco White <mwhite494@gmail.com>
2018-03-20 01:27:44 -0400

    Add files via upload

---
+++ adxl335.ino
@@ -0,0 +1,51 @@
+// these constants describe the pins. They won't change:
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int powerpin = 19;              // analog input pin 5 -- voltage
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)
+
+// Include LCD library
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(9600);
+
+  // Provide ground and power by using the analog inputs as normal digital pins.
+  // This makes it possible to directly connect the breakout board to the
+  // Arduino. If you use the normal 5V and GND pins on the Arduino,
+  // you can remove these lines.
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
+
+  // Initialize LCD screen
+  lcd.begin(16,2);
+  lcd.print(" X   Y   Z");
+}
+
+void loop() {
+  // print the sensor values:
+  Serial.print(analogRead(xpin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(ypin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(zpin));
+  Serial.println();
+  // Print to the LCD screen
+  lcd.setCursor(0,1);
+  lcd.print("           ");
+  lcd.setCursor(0,1);
+  lcd.print(String(analogRead(xpin))+" "+String(analogRead(ypin))+" "+String(analogRead(zpin)));
+  // delay before next reading:
+  delay(100);
+}
---
+++ vl53l0x_test.ino
@@ -0,0 +1,48 @@
+#include "Adafruit_VL53L0X.h"
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(115200);
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+  lcd.print("Distance (mm):");
+}
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+  
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): ");
+    Serial.println(String(measure.RangeMilliMeter));
+    lcd.setCursor(0,1);
+    lcd.print("    ");
+    lcd.setCursor(0,1);
+    lcd.print(String(measure.RangeMilliMeter));
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+}
